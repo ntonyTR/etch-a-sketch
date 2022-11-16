f2976f48667e19c7117ec8572d2bc488a508ac49
Antony TR <antonyetr@outlook.com>
2022-11-15 23:46:14 -0600

    Event listeners implemented, divs appears onclick

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let helloWorld = document.createElement("p");
 const container = document.getElementById("container");
-helloWorld.textContent = "Hello World";
-container.appendChild(helloWorld);
+
+const button4 = document.getElementById("btn_4");
+const button6 = document.getElementById("btn_6");
+const button8 = document.getElementById("btn_8");
+
+button4.addEventListener("click", () => makeGrid(17));
+button6.addEventListener("click", () => makeGrid(37));
+button8.addEventListener("click", () => makeGrid(65));
+
+function makeGrid(n) {
+  for (let i = 1; i < n; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid");
+    container.appendChild(div);
+    div.textContent = i;
+  }
+}
